cf5bbbcbb8273cd384777c92e1e7b871a52ee8e4
Bruno <brunorockbc39@gmail.com>
2025-04-13 13:44:56 +0000

    ainda está aparecendo a mensagem em Recomendações de Otimização

--- src/ai/ai-instance.ts
+++ src/ai/ai-instance.ts
@@ -5,7 +5,7 @@ export const ai = genkit({
   promptDir: './prompts',
   plugins: [
     googleAI({
-      apiKey: process.env.GOOGLE_GENAI_API_KEY,
+      apiKey: process.env.GOOGLE_GENAI_API_KEY || 'AIzaSyCQogB99Oll-s7P_DwXoBSpAHsXgbi9Qiw',
     }),
   ],
   model: 'googleai/gemini-2.0-flash',
