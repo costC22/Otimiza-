fc249e788937b5565c418445005c995ffa2f6da3
Bruno <brunorockbc39@gmail.com>
2025-04-14 13:44:21 +0000

    404 This page could not be found. está ficando assim

--- next.config.ts
+++ next.config.ts
@@ -10,17 +10,17 @@ const nextConfig: NextConfig = {
   },
   
   // Ensure that all routes are correctly handled.
-  async rewrites() {
-    return [
-      {
-        source: '/:path*',
-        destination: '/:path*',
-      },
-    ];
-  },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: '/:path*',
+  //       destination: '/:path*',
+  //     },
+  //   ];
+  // },
 
-  output: 'export', // Specify that the app should be exported as static HTML
-  distDir: 'out', //  Specify the output directory
+  // output: 'export', // Specify that the app should be exported as static HTML
+  // distDir: 'out', //  Specify the output directory
 };
 
 export default nextConfig;
