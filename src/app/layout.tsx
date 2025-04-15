f33b676569943e76938602a68dc401fa56a9c230
Bruno <brunorockbc39@gmail.com>
2025-04-15 15:06:32 +0000

    retire por favor

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,6 @@
 
 import './globals.css';
 import { Geist } from 'next/font/google';
-import { ListChecks, Rocket, Settings } from 'lucide-react';
-import { Sidebar, SidebarContent, SidebarMenu, SidebarMenuItem, SidebarProvider } from "@/components/ui/sidebar";
 import {metadata} from './metadata';
 import React from "react";
 
@@ -11,45 +9,6 @@ const geistSans = Geist({
   variable: '--font-geist-sans',
   subsets: ['latin'],
 });
-
-const ClientSideSidebar = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <SidebarProvider>
-      <Sidebar>
-        <SidebarContent>
-          
-            
-              <SidebarMenuItem href="/">
-                <Settings className="mr-2 h-4 w-4" />
-                Painel de Controle
-              </SidebarMenuItem>
-            
-            
-              <SidebarMenuItem href="/system-analysis">
-                <Settings className="mr-2 h-4 w-4" />
-                Análise do Sistema
-              </SidebarMenuItem>
-            
-            
-              <SidebarMenuItem href="/recommendations">
-                <ListChecks className="mr-2 h-4 w-4" />
-                Recomendações
-              </SidebarMenuItem>
-            
-            
-              <SidebarMenuItem href="/auto-optimize">
-                <Rocket className="mr-2 h-4 w-4" />
-                Otimização Automática
-              </SidebarMenuItem>
-            
-          
-          
-        </SidebarContent>
-      </Sidebar>
-      {children}
-    </SidebarProvider>
-  );
-};
 
 const RootLayout = ({
   children,
@@ -60,11 +19,9 @@ const RootLayout = ({
     <html lang="pt-BR">
       <body className={geistSans.className}>
         
-          <ClientSideSidebar>
             
               {children}
             
-          </ClientSideSidebar>
         
       
       </body>
@@ -73,4 +30,5 @@ const RootLayout = ({
 }
 
 export default RootLayout;
+
 
